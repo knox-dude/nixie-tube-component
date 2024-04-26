c145b99b07310c38b08e23bf82a28a82ccd99300
Andrew Knox <andrew.knox.r@gmail.com>
2024-04-26 11:22:30 -0700

    changing app location, updating main

---
+++ src/Components/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="font-chocolate">0I23456789</h1>
+      <h1 className="font-sans">0I23456789</h1>
+      <h1 className="">0I23456789</h1>
+    </div>
+  )
+}
+
+export default App
